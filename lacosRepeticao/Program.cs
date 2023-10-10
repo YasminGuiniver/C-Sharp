fe88b7e08cf886d50fef176992343d80a274ef3e
Yasmin Guiniver <94757345+YasminGuiniver@users.noreply.github.com>
2023-10-10 17:06:58 -0300

    Laços de repetição
    Exemplos de códigos usando laços de repetição com C#

---
+++ lacosRepeticao/Program.cs
@@ -0,0 +1,13 @@
+﻿//LAÇOS DE REPETIÇÃO
+
+var n1 = 1;
+while(n1 <= 15)
+{
+    Console.WriteLine(n1);
+    n1++;
+} //ENQUANTO N1 FOR MENOR OU IGUAL A 15 ESCREVA ELE POR LINHA, A CADA LINHA ADICIONE MAIS UM
+
+for(int n2 = 2; n2 <= 10; n2++)
+{
+    Console.WriteLine(n2);
+}
